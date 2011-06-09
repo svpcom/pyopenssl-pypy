52116e1613826c2544895c418f6258acf1de4ff9
Vasiliy Evseenko <vasiliy.evseenko@qik.com>
2011-06-09 11:56:58 +0400

    Initial commit

---
+++ test/server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#
+
+from twisted.internet import ssl, reactor
+from twisted.internet.protocol import Factory, Protocol
+
+class Echo(Protocol):
+    def dataReceived(self, data):
+        """As soon as any data is received, write it back."""
+        self.transport.write(data)
+
+if __name__ == '__main__':
+    factory = Factory()
+    factory.protocol = Echo
+    reactor.listenSSL(8000, factory,
+                      ssl.DefaultOpenSSLContextFactory(
+            'server.key', 'server.crt'))
+    reactor.run()
